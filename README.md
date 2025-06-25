fa11a6ab28c2bbe4e7daa07b08b9db10010302f7
SqrtNegativOne <sqrtnegativ1@gmail.com>
2025-06-25 10:54:29 +0530

    reorganise and update readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # AI-factcheck
 Crude, simple, and modular AI factchecking implementation.
 
+Scripts, testing files, and research files are in the /scripts/ directory.
+
+Main program can be found at /src/.
+
+Pipeline for old-main.py. (no RAG, no decontextualisation or falsifiability checks)
 ![Pipeline](./meta/diagram.png)
+
+Pipeline for main.py: in progress.
--- src/main.py
+++ src/main.py
@@ -7,10 +7,6 @@ import pickle
 from pydantic import BaseModel, Field, HttpUrl
 from abc import ABC, abstractmethod
 
-from langchain.prompts import PromptTemplate
-from langchain_ollama import OllamaLLM
-from langchain.output_parsers import PydanticOutputParser
-
 import serpapi
 import re
 
@@ -24,40 +20,9 @@ from bs4 import BeautifulSoup
 import random
 import newspaper
 
+from utils import *
+import claim_extraction_models
 
-DEBUG_MODE: bool = True
-DEMO_MODE: bool = False # if True, the program will use hardcoded text instead of loading it from URLs
-
-BASE_DIR: Path = Path(__file__).parent
-RELIABLE_SOURCES_PATH: Path = BASE_DIR / "data" / "reliable-sources.pkl"
-SAMPLE_INPUT_PATH: Path = BASE_DIR / "data" / "sample-input.txt"
-SAMPLE_SOURCE_TEXTS_PATH: Path = BASE_DIR / "data" / "sample-source-texts.txt"
-CLAIM_EXTRACTION_TEMPLATE_PATH: Path = BASE_DIR / "data" / "claim-extraction-template.txt"
-
-load_dotenv(BASE_DIR / ".env")
-SERPAPI_KEY: str = os.getenv("SERPAPI_KEY", "")
-
-INPUT_URL = "https://edition.cnn.com/2023/10/29/sport/nfl-week-8-how-to-watch-spt-intl/index.html" #"https://www.theguardian.com/science/brain-flapping/2014/nov/25/climate-change-is-an-obvious-myth-how-much-more-evidence-do-you-need" # must not be same as EXAMPLE_URL
-EXAMPLE_URL = "https://example.com"
-
-
-class Relation(Enum):
-    ENTAILMENT = "entailment"
-    CONTRADICTION = "contradiction"
-    NEUTRAL = "neutral"
-
-class Contradiction(BaseModel):
-    claim: str
-    source_url: HttpUrl
-    source_claim: str
-
-class Claims(BaseModel):
-    claims: list[str] = Field(description="List of claims extracted from the text.")
-
-class ClaimExtractor(ABC):
-    @abstractmethod
-    def extract_claims(self, text: str) -> list[str]:
-        pass
 
 class SourcesFinder(ABC):
     def __init__(self) -> None:
@@ -76,43 +41,6 @@ class NLIModel(ABC):
     @abstractmethod
     def recognise_textual_entailment(self, premise: str, hypothesis: str) -> Relation:
         pass
-
-
-class OllamaClaimExtractor(ClaimExtractor):
-    def __init__(self, model_name: str) -> None:
-        with open(CLAIM_EXTRACTION_TEMPLATE_PATH, "r", encoding="utf-8") as f:
-            CLAIM_EXTRACTION_TEMPLATE: str = f.read()
-        prompt = PromptTemplate.from_template(CLAIM_EXTRACTION_TEMPLATE)
-
-        llm = OllamaLLM(model=model_name, temperature=0.0)
-        self.parser = PydanticOutputParser(pydantic_object=Claims)
-        self.chain = prompt | llm | self.parser
-
-    def extract_claims(self, text: str) -> list[str]:
-
-        if text.strip() == "":
-            if DEBUG_MODE:
-                print("=> No text provided for claim extraction. Returning empty list.")
-            return []
-
-        claims: list[str] = self.chain.invoke({"text": text, "format_instructions": self.parser.get_format_instructions()}).claims
-
-        # Cleaning up claims
-        dont_start_with = ['-', '- ', 'The ', 'speaker ', 'text ', 'post ', 'article ', 'blog post ', 'blog article ', 'blog post article ']
-        for substring in dont_start_with:
-            if all(claim.startswith(substring) for claim in claims):
-                claims = [claim[len(substring):] for claim in claims]
-        
-        if all(claim.endswith(".") for claim in claims):
-            claims = [claim[:-1] for claim in claims]
-        
-        claims = [claim.strip() for claim in claims if claim.strip()]
-
-        if DEBUG_MODE:
-            print(f"\n=> Cleaned claims from the text:\n")
-            print_list(claims)
-        
-        return claims
     
 class SerpApiSourcesFinder(SourcesFinder):
     def find_sources(self, claim: str) -> list[str]:
@@ -157,7 +85,7 @@ class HuggingFaceNLIModel(NLIModel):
         with torch.no_grad():
             logits = self.model(**inputs).logits
             probs = F.softmax(logits, dim=-1)
-        return (Relation.CONTRADICTION, Relation.NEUTRAL, Relation.ENTAILMENT)[probs.argmax().item()]
+        return [Relation.CONTRADICTION, Relation.NEUTRAL, Relation.ENTAILMENT][int(probs.argmax().item())]
 
 
 def load_text(url: str) -> str:
@@ -204,14 +132,6 @@ def load_text(url: str) -> str:
 
     return text
 
-def print_list(lst: list[str]) -> None:
-    if not lst:
-        print("NO ITEMS FOUND")
-        return
-    for i, v in enumerate(lst, start=1):
-        print(f"{i}. {v}")
-    print()
-
 def findings(claims: list[str], sources_urls: set[str], contradictions: list[Contradiction], unverified_veracities: list[int], source_claim_count: int) -> None:
     print("\nClaims made by the article:")
     print_list(claims)
@@ -240,7 +160,7 @@ def findings(claims: list[str], sources_urls: set[str], contradictions: list[Con
 
 def main():
     # Load models
-    claim_extractor: ClaimExtractor = OllamaClaimExtractor(model_name='qwen:7b-chat')
+    claim_extractor: claim_extraction_models.ClaimExtractor = claim_extraction_models.OllamaClaimExtractor(model_name='qwen:7b-chat')
     if DEMO_MODE:
         sources_finder: SourcesFinder = DemoSourcesFinder()
     else:
